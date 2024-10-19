4b245b4078c53ead6cb326d974f95734f50a32d7
parksujong <tnwhd92@naver.com>
2024-10-19 21:39:02 +0900

    first commit

---
+++ src/homeTestPackages/homeTestClass.java
@@ -0,0 +1,11 @@
+package homeTestPackages;
+
+public class homeTestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("안녕하세요.");
+
+	}
+
+}
