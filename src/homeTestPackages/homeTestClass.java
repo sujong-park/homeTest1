e36cc29a76f31b74b5a0f40b3d5a6ffafa11639e
parksujong <tnwhd92@naver.com>
2024-10-19 21:47:18 +0900

    업로드~

--- src/homeTestPackages/homeTestClass.java
+++ src/homeTestPackages/homeTestClass.java
@@ -5,6 +5,7 @@ public class homeTestClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("안녕하세요.");
+		System.out.println("안녕하세요.");
 
 	}
 
